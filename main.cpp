c338c2016c9d3efdb92f4ad114c55a671173f761
kris.leshukova <kris.leshukova@gmail.com>
2022-12-21 01:11:05 +0700

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+double my_pow(double x, unsigned int y) {
+    if (!y) {
+        return 1;
+    }
+    for (int i = 0; i < y - 1; ++i) {
+        x = x*x;
+    }
+    return x;
+
+}
+
+int main()
+{
+    std::cout << my_pow(3.0, 2) << std::endl;
+}
